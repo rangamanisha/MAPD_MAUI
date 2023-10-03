f1ff4bbd7b8427905fe0d6eb02f76a85a81dcc6b
Manisha Ranga <rangamanisha11@gmail.com>
2023-10-03 12:03:56 -0400

    Simple calculator only UI without logic

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleCalculator
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SimpleCalculator/calculate.cs
@@ -0,0 +1,15 @@
+namespace SimpleCalculator;
+
+public class calculate : ContentPage
+{
+	public calculate()
+	{
+		Content = new VerticalStackLayout
+		{
+			Children = {
+				new Label { HorizontalOptions = LayoutOptions.Center, VerticalOptions = LayoutOptions.Center, Text = "Welcome to .NET MAUI!"
+				}
+			}
+		};
+	}
+}
